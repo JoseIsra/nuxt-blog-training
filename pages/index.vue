9bd1b27e1805bc331244137168d0b841cbdc3629
JoseIsra <israelmyname@gmail.com>
2023-08-15 22:27:28 -0500

    ✨ (setup): Setup project
    setup project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Hola mundo nuxt 2 con vuejs 2 👻</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
